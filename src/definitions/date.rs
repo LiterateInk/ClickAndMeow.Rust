4e0d1b9faf7f156b728de99137b39ffef9d99627
Maël GUERIN <mael.guerin@murena.io>
2025-01-10 11:00:27 +0100

    fix(date): refactor

--- src/definitions/date.rs
+++ src/definitions/date.rs
@@ -1,35 +1,16 @@
-#[cfg(target_arch = "wasm32")]
-#[wasm::export]
+#[cfg_attr(target_arch = "wasm32", wasm::export)]
+#[cfg_attr(feature = "ffi", derive(uniffi::Object))]
 pub struct Date {
     pub year: i32,
     pub month: i32,
     pub day: i32
 }
 
-#[cfg(not(target_arch = "wasm32"))]
-pub struct Date {
-    pub year: i32,
-    pub month: i32,
-    pub day: i32
-}
-
-#[cfg(target_arch = "wasm32")]
-#[wasm::export]
-impl Date {
-    #[cfg_attr(target_arch = "wasm32", wasm_bindgen(constructor))]
-    pub fn new(year: i32, month: i32, day: i32) -> Self {
-        Self {
-            year,
-            month,
-            day
-        }
-    }
-}
-
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg_attr(target_arch = "wasm32", wasm::export)]
 #[cfg_attr(feature = "ffi", uniffi::export)]
 impl Date {
     #[cfg_attr(feature = "ffi", uniffi::constructor)]
+    #[cfg_attr(target_arch = "wasm32", wasm_bindgen(constructor))]
     pub fn new(year: i32, month: i32, day: i32) -> Self {
         Self {
             year,

c3fa75722b3a91e7af52eaecb0d6a60ce41ebd48
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-04-06 03:04:02 +0200

    chore: cleanup everything !

--- build.gradle.kts
+++
@@ -1,4 +0,0 @@
-plugins {
-  id("org.jetbrains.kotlin.multiplatform") version "2.0.0" apply false
-  id("com.android.library") version "8.3.0" apply false
-}
--- src/bin/uniffi-bindgen.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-  #[cfg(feature = "ffi")]
-  uniffi::uniffi_bindgen_main()
-}
